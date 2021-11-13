0de745cb87f132d6915740b7ae14553c7ef2d284
Akargzom <83316201+Akargzom@users.noreply.github.com>
2021-11-13 20:08:53 +0200

    Update mega-giga-tabs.php

--- mega-giga-tabs.php
+++ mega-giga-tabs.php
@@ -8,7 +8,7 @@
  * @wordpress-plugin
  * Plugin Name:       Mega-giga-tabs
  * Description:       Making your tabs mega-giga!!! requires Smart Custom Fields 4.2.0
- * Version:           1.1.001b
+ * Version:           1.1.001c
  * Requires PHP:      5.6
  * Requires plugins:  Smart Custom Fields 4.2.0
  * Author:            Sashko
