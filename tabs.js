792ca5d69a511b12b989ddb6431342d4c98f07db
Akargzom <83316201+Akargzom@users.noreply.github.com>
2021-11-13 18:27:51 +0200

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
     let tab = document.querySelectorAll('.tab'),
-        tabContent = document.querySelectorAll('.tab-cont-inner');
+        tabContent = document.querySelectorAll('.tab-cont'),
+        tabContentInner = document.querySelectorAll('.tab-cont-inner');
     if (window.matchMedia("(min-width: 1024px)").matches) {
         for (let i = 0; i < tabContent.length; i++) {
             if (tabContent[i].classList.contains('show')) {
@@ -10,8 +11,8 @@ window.addEventListener('DOMContentLoaded', function () {
                 if (bigTab.classList.contains('tabs-block-h')) {
                     bigTab.style.height = tab[i].clientHeight + 35 + tabContent[i].clientHeight + 'px';
                 } else {
-                    if (tabs.clientHeight < tabContent[i].clientHeight) {
-                        bigTab.style.height = tabContent[i].clientHeight + 'px';
+                    if (tabs.clientHeight < (tabContentInner[i].clientHeight+60)) {
+                        bigTab.style.height = (tabContentInner[i].clientHeight+60) + 'px';
                     } else {
                         bigTab.style.height = tabs.clientHeight + 'px';
                         tabContent[i].style.height = bigTab.clientHeight + 'px';
@@ -21,24 +22,27 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
     window.addEventListener('resize', function () {
-        for (let i = 0; i < tabContent.length; i++) {
-            if (tabContent[i].classList.contains('show')) {
-                let bigTab = tabContent[i].parentNode.parentNode.parentNode,
-                    tabs = tabContent[i].parentNode.parentNode;
-                if (bigTab.classList.contains('tabs-block-h')) {
-                    bigTab.style.height = tab[i].clientHeight + 35 + tabContent[i].clientHeight + 'px';
-                } else {
-                    if (tabs.clientHeight < tabContent[i].clientHeight) {
-                        bigTab.style.height = tabContent[i].clientHeight + 'px';
+        if (window.matchMedia("(min-width: 1024px)").matches) {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (tabContent[i].classList.contains('show')) {
+                    let bigTab = tabContent[i].parentNode.parentNode.parentNode,
+                        tabs = tabContent[i].parentNode.parentNode;
+                    if (bigTab.classList.contains('tabs-block-h')) {
+                        bigTab.style.height = tab[i].clientHeight + 35 + tabContent[i].clientHeight + 'px';
                     } else {
-                        bigTab.style.height = tabs.clientHeight + 'px';
-                        tabContent[i].style.height = tabs.clientHeight + 'px';
+                        if (tabs.clientHeight < (tabContentInner[i].clientHeight+60)) {
+                            bigTab.style.height = (tabContentInner[i].clientHeight+60) + 'px';
+                        } else {
+                            bigTab.style.height = tabs.clientHeight + 'px';
+                            tabContent[i].style.height = tabs.clientHeight + 'px';
+                        }
                     }
                 }
             }
         }
     });
     if (window.matchMedia("(max-width: 1023px)").matches) {
+        console.log( tabContent );
         for (let i = 0; i < tab.length; i++) {
             if (tab[i].classList.contains('active')) {
                 tab[i].classList.remove('active');
@@ -51,7 +55,7 @@ window.addEventListener('DOMContentLoaded', function () {
             let bigTab = this.parentNode.parentNode,
                 innerTabs = this.parentNode,
                 innerTab = bigTab.querySelectorAll('.tab'),
-                innerTabContent = bigTab.querySelectorAll('.tab-cont-inner');
+                innerTabContent = bigTab.querySelectorAll('.tab-cont');
             for (let i = 0; i < innerTab.length; i++) {
                 if (innerTab[i] == this) {
                     innerTab[i].classList.add('active');
