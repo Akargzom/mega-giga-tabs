e5e572875b719d7ccfabf8f29770e07d056e08b9
Akargzom <83316201+Akargzom@users.noreply.github.com>
2021-11-15 13:24:38 +0200

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -6,13 +6,13 @@ window.addEventListener('DOMContentLoaded', function () {
     if (window.matchMedia("(min-width: 1024px)").matches) {
         for (let i = 0; i < tabContent.length; i++) {
             if (tabContent[i].classList.contains('show')) {
-                let bigTab = tabContent[i].parentNode.parentNode.parentNode,
-                    tabs = tabContent[i].parentNode.parentNode;
+                let bigTab = tabContent[i].closest('.bigtab'),
+                    tabs = tabContent[i].closest('.tabs');
                 if (bigTab.classList.contains('tabs-block-h')) {
                     bigTab.style.height = tab[i].clientHeight + 35 + tabContent[i].clientHeight + 'px';
                 } else {
-                    if (tabs.clientHeight < (tabContentInner[i].clientHeight+60)) {
-                        bigTab.style.height = (tabContentInner[i].clientHeight+60) + 'px';
+                    if (tabs.clientHeight < (tabContentInner[i].clientHeight + 60)) {
+                        bigTab.style.height = (tabContentInner[i].clientHeight + 60) + 'px';
                     } else {
                         bigTab.style.height = tabs.clientHeight + 'px';
                         tabContent[i].style.height = bigTab.clientHeight + 'px';
@@ -25,13 +25,13 @@ window.addEventListener('DOMContentLoaded', function () {
         if (window.matchMedia("(min-width: 1280px)").matches) {
             for (let i = 0; i < tabContent.length; i++) {
                 if (tabContent[i].classList.contains('show')) {
-                    let bigTab = tabContent[i].parentNode.parentNode.parentNode,
-                        tabs = tabContent[i].parentNode.parentNode;
+                    let bigTab = tabContent[i].closest('.bigtab'),
+                        tabs = tabContent[i].closest('.tabs');
                     if (bigTab.classList.contains('tabs-block-h')) {
                         bigTab.style.height = tab[i].clientHeight + 35 + tabContent[i].clientHeight + 'px';
                     } else {
-                        if (tabs.clientHeight < (tabContentInner[i].clientHeight+60)) {
-                            bigTab.style.height = (tabContentInner[i].clientHeight+60) + 'px';
+                        if (tabs.clientHeight < (tabContentInner[i].clientHeight + 60)) {
+                            bigTab.style.height = (tabContentInner[i].clientHeight + 60) + 'px';
                         } else {
                             bigTab.style.height = tabs.clientHeight + 'px';
                             tabContent[i].style.height = tabs.clientHeight + 'px';
@@ -42,7 +42,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
     if (window.matchMedia("(max-width: 1279px)").matches) {
-        console.log( tabContent );
+        console.log(tabContent);
         for (let i = 0; i < tab.length; i++) {
             if (tab[i].classList.contains('active')) {
                 tab[i].classList.remove('active');
@@ -52,8 +52,8 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     for (let i = 0; i < tab.length; i++) {
         tab[i].addEventListener('click', function () {
-            let bigTab = this.parentNode.parentNode,
-                innerTabs = this.parentNode,
+            let bigTab = this.closest('.bigtab'),
+                innerTabs = this.closest('.tabs'),
                 innerTab = bigTab.querySelectorAll('.tab'),
                 innerTabContent = bigTab.querySelectorAll('.tab-cont');
             for (let i = 0; i < innerTab.length; i++) {
@@ -67,8 +67,8 @@ window.addEventListener('DOMContentLoaded', function () {
                             bigTab.style.height = innerTabContent[i].clientHeight + 'px';
                         } else {
                             if (window.matchMedia("(min-width: 1280px)").matches) {
-                            bigTab.style.height = innerTabs.clientHeight + 'px';
-                            innerTabContent[i].style.height = bigTab.clientHeight + 'px';
+                                bigTab.style.height = innerTabs.clientHeight + 'px';
+                                innerTabContent[i].style.height = bigTab.clientHeight + 'px';
                             }
                         }
                     }
