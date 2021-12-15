6b118a1702adf09f48588a5738288c8c28d19b34
Alexander Ilyashenko <zhdi.2011@gmail.com>
2021-12-15 02:33:45 +0200

    Update mega-giga-tabs.php

--- mega-giga-tabs.php
+++ mega-giga-tabs.php
@@ -8,22 +8,20 @@
  * @wordpress-plugin
  * Plugin Name:       Mega-giga-tabs
  * Description:       Making your tabs mega-giga!!! requires Smart Custom Fields 4.2.0
- * Version:           1.2.001
+ * Version:           1.2.002
  * Requires PHP:      5.6
  * Author:            Sashko
  */
-add_action('init', 'include_css_js');
-function include_css_js()
+add_action('wp_enqueue_scripts', function()
 {
-    wp_enqueue_style('tabs', plugins_url('mega-giga-tabs/tabs.css'), []);
-    wp_enqueue_script('tabs', plugins_url('mega-giga-tabs/tabs.js'), []);
-}
+    wp_enqueue_style('tabs', plugins_url('tabs.css', __FILE__ ));
+    wp_enqueue_script('tabs', plugins_url('tabs.js', __FILE__ ));
+});
 
-add_action('init', 'tabs_type_reg');
-function tabs_type_reg()
+add_action('plugins_loaded', function()
 {
-    define('PL_T1', 'tabs');
-    register_post_type(PL_T1, [
+    define('MGTZ_PL_T1', 'tabs');
+    register_post_type(MGTZ_PL_T1, [
         'labels' => [
             'name'               => 'Tabs',
             'singular_name'      => 'Tab',
@@ -46,22 +44,22 @@ function tabs_type_reg()
         'publicly_queryable' => false,
         'supports'           => ['title', 'editor'],
     ]);
-}
+});
 
-add_action('init', 'tabs_scf', 20);
-function tabs_scf()
+add_action('smart-cf-register-fields', 'MGTZ_tabs_scf', 20);
+function MGTZ_tabs_scf()
 {
     if (class_exists('SCF')) {
-        add_filter('smart-cf-register-fields', 'tabs_add_meta_fields', 10, 5);
-        function tabs_add_meta_fields($settings, $type, $id, $meta_type, $types)
+        add_filter('smart-cf-register-fields', 'MGTZ_tabs_add_meta_fields', 10, 5);
+        function MGTZ_tabs_add_meta_fields($settings, $type, $id, $meta_type, $types)
         {
             $settings = [];
-            if ($type == PL_T1) {
+            if ($type == MGTZ_PL_T1) {
                 $setting = SCF::add_setting('tabs_scf', 'Mega-giga-tabs!!!');
                 $setting->add_group('tabs autoscroll', false, [
                 [
                     'type' => 'boolean',
-                    'name' => 'ascrl',
+                    'name' => 'MGTZ_ascrl',
                     'label' => 'Autoscroll',
                     'true_label' => 'On',
                     'false_label' => 'Off',
@@ -72,7 +70,7 @@ function tabs_scf()
                 $setting->add_group('tabs form', false, [
                     [
                         'type' => 'select',
-                        'name' => 'tfs',
+                        'name' => 'MGTZ_tfs',
                         'label' => 'tabs form',
                         'choices' => [
                             'horizontal' => 'horizontal tabs',
@@ -83,25 +81,25 @@ function tabs_scf()
                 $setting->add_group('tabs content', true, [
                     [
                         'type' => 'text',
-                        'name' => 'th',
+                        'name' => 'MGTZ_th',
                         'label' => 'Tab header',
                     ],                     [
                         'type' => 'text',
-                        'name' => 'tex',
+                        'name' => 'MGTZ_tex',
                         'label' => 'Tab excerpt',
                     ], [
                         'type' => 'image',
-                        'name' => 'tth',
+                        'name' => 'MGTZ_tth',
                         'label' => 'Tab thumbnail',
                         'size' => 'thumbnail',
                     ], [
                         'type' => 'image',
-                        'name' => 'atth',
+                        'name' => 'MGTZ_atth',
                         'label' => 'Active tab thumbnail',
                         'size' => 'thumbnail',
                     ], [
                         'type' => 'wysiwyg',
-                        'name' => 'ttc',
+                        'name' => 'MGTZ_ttc',
                         'label' => 'Active tab text content',
                     ],
 
@@ -112,32 +110,29 @@ function tabs_scf()
         }
     }
 }
-add_action('admin_head', 'true_add_mce_button');
-function true_add_tinymce_script($plugin_array)
+add_action('admin_head', 'MGTZ_true_add_mce_button');
+function MGTZ_true_add_tinymce_script($plugin_array)
 {
     $plugin_array['true_mce_button'] = get_stylesheet_directory_uri() . '/button.js';
     return $plugin_array;
 }
-function true_add_mce_button()
+function MGTZ_true_add_mce_button()
 {
     if (!current_user_can('edit_posts') && !current_user_can('edit_pages')) {
         return;
     }
     if ('true' == get_user_option('rich_editing')) {
-        wp_enqueue_script('button', plugins_url('mega-giga-tabs/button.js'), []);
-        add_filter('mce_external_plugins', 'true_add_tinymce_script');
-        add_filter('mce_buttons', 'true_register_mce_button');
+        wp_enqueue_script('button', plugins_url('button.js', __FILE__));
+        add_filter('mce_external_plugins', 'MGTZ_true_add_tinymce_script');
+        add_filter('mce_buttons', 'MGTZ_true_register_mce_button');
     }
 }
-function true_register_mce_button($buttons)
+function MGTZ_true_register_mce_button($buttons)
 {
     array_push($buttons, 'true_mce_button');
     return $buttons;
 }
-add_action('init', 'add_sc');
-function add_sc()
-{
-    wp_enqueue_style('tabs', '/tabs.css');
+add_action('plugins_loaded', function(){
     add_shortcode('tabz', 'get_tabs');
     function get_tabs($atts)
     {
@@ -146,61 +141,61 @@ function add_sc()
         ], $atts);
         $autoscroll = ( in_array('autoscroll', $atts) ) ?: false;
         $content = get_posts([
-            'post_type' => PL_T1,
+            'post_type' => MGTZ_PL_T1,
             'title' => $a['title'],
             'numberposts' => 1
         ])[0];
         $fields = get_post_custom($content->ID);
-        if($fields['ascrl'][0] == true){
-            $ascrl = ' auto';
+        if($fields['MGTZ_ascrl'][0] == true){
+            $MGTZ_ascrl = ' MGTZ_auto';
         }else{
-            $ascrl = '';
+            $MGTZ_ascrl = '';
         }
-        if (isset($fields['tfs'][0]) && $fields['tfs'][0] == 'horizontal') {
-            $html = "<div class='tabs-block-h bigtab".$ascrl."'><div class='tabs-h tabs'>";
-            foreach ($fields['th'] as $i => $th) {
-                $html .=  "<div class='tab-h";
-                if ($i == 0) $html .= ' active';
-                $html .= " tab'><div class='shadow'>
-                        <div class='img'>";
-                if (isset($fields['tth'][$i]) && !empty($fields['tth'][$i]))
-                    $html .=  "<img class ='def' src='" . wp_get_attachment_image_url($fields['tth'][$i], 'full') . "' alt=''>";
-                if (isset($fields['atth'][$i]) && !empty($fields['atth'][$i]))
-                    $html .=  "<img class='act' src='" . wp_get_attachment_image_url($fields['atth'][$i], 'full') . "' alt=''>";
-                $html .= "</div><div class='text'>";
-                if (isset($fields['th'][$i]) && !empty($fields['th'][$i]))
+        if (isset($fields['MGTZ_tfs'][0]) && $fields['MGTZ_tfs'][0] == 'horizontal') {
+            $html = "<div class='MGTZ_tabs-block-h MGTZ_bigtab".$MGTZ_ascrl."'><div class='MGTZ_tabs-h MGTZ_tabs'>";
+            foreach ($fields['MGTZ_th'] as $i => $th) {
+                $html .=  "<div class='MGTZ_tab-h MGTZ_tab";
+                if ($i == 0) $html .= ' MGTZ_active';                    
+                $html .= " tab'><div class='MGTZ_shadow'>
+                        <div class='MGTZ_img'>";
+                if (isset($fields['MGTZ_tth'][$i]) && !empty($fields['MGTZ_tth'][$i]))
+                    $html .=  "<img class ='MGTZ_def' src='" . wp_get_attachment_image_url($fields['MGTZ_tth'][$i], 'full') . "' alt=''>";
+                if (isset($fields['MGTZ_atth'][$i]) && !empty($fields['MGTZ_atth'][$i]))
+                    $html .=  "<img class='MGTZ_act' src='" . wp_get_attachment_image_url($fields['MGTZ_atth'][$i], 'full') . "' alt=''>";
+                $html .= "</div><div class='MGTZ_text'>";
+                if (isset($fields['MGTZ_th'][$i]) && !empty($fields['MGTZ_th'][$i]))
                     $html .= "<h2>" . $th . "</h2>";
-                if (isset($fields['tex'][$i]) && !empty($fields['tex'][$i]))
-                    $html .= "<p>" . $fields['tex'][$i] . "</p>";
+                if (isset($fields['MGTZ_tex'][$i]) && !empty($fields['MGTZ_tex'][$i]))
+                    $html .= "<p>" . $fields['MGTZ_tex'][$i] . "</p>";
                 $html .= "</div>
                     </div>
-                    <div class='tab-cont";
-                if ($i == 0) $html .= ' show';
-                $html .= "'><div class='tab-cont-inner'>" . $fields['ttc'][$i] . "</div></div></div>";
+                    <div class='MGTZ_tab-cont";
+                if ($i == 0) $html .= ' MGTZ_show';
+                $html .= "'><div class='MGTZ_tab-cont-inner'>" . $fields['MGTZ_ttc'][$i] . "</div></div></div>";
             }
             $html .= "</div></div>";
             return $html;
-        } elseif (isset($fields['tfs'][0]) && $fields['tfs'][0] == 'vertical') {
-            $html = "<div class='tabs-block-v bigtab".$ascrl."'><div class='tabs-v  tabs'>";
-            foreach ($fields['th'] as $i => $th) {
-                $html .=  "<div class='tab-v";
-                if ($i == 0) $html .= ' active';
-                $html .= " tab'><div class='shadow'>
-                    <div class='img'>";
-                if (isset($fields['tth'][$i]) && !empty($fields['tth'][$i]))
-                    $html .=  "<img class ='def' src='" . wp_get_attachment_image_url($fields['tth'][$i], 'full') . "' alt=''>";
-                if (isset($fields['atth'][$i]) && !empty($fields['atth'][$i]))
-                    $html .=  "<img class='act' src='" . wp_get_attachment_image_url($fields['atth'][$i], 'full') . "' alt=''>";
-                $html .= "</div><div class='text'>";
-                if (isset($fields['th'][$i]) && !empty($fields['th'][$i]))
+        } elseif (isset($fields['MGTZ_tfs'][0]) && $fields['MGTZ_tfs'][0] == 'vertical') {
+            $html = "<div class='MGTZ_tabs-block-v MGTZ_bigtab".$MGTZ_ascrl."'><div class='MGTZ_tabs-v  MGTZ_tabs'>";
+            foreach ($fields['MGTZ_th'] as $i => $th) {
+                $html .=  "<div class='MGTZ_tab-v MGTZ_tab";
+                if ($i == 0) $html .= ' MGTZ_active';
+                $html .= " tab'><div class='MGTZ_shadow'>
+                    <div class='MGTZ_img'>";
+                if (isset($fields['MGTZ_tth'][$i]) && !empty($fields['MGTZ_tth'][$i]))
+                    $html .=  "<img class ='MGTZ_def' src='" . wp_get_attachment_image_url($fields['MGTZ_tth'][$i], 'full') . "' alt=''>";
+                if (isset($fields['MGTZ_atth'][$i]) && !empty($fields['MGTZ_atth'][$i]))
+                    $html .=  "<img class='MGTZ_act' src='" . wp_get_attachment_image_url($fields['MGTZ_atth'][$i], 'full') . "' alt=''>";
+                $html .= "</div><div class='MGTZ_text'>";
+                if (isset($fields['MGTZ_th'][$i]) && !empty($fields['MGTZ_th'][$i]))
                     $html .= "<h2>" . $th . "</h2>";
-                if (isset($fields['tex'][$i]) && !empty($fields['tex'][$i]))
-                    $html .= "<p>" . $fields['tex'][$i] . "</p>";
+                if (isset($fields['MGTZ_tex'][$i]) && !empty($fields['MGTZ_tex'][$i]))
+                    $html .= "<p>" . $fields['MGTZ_tex'][$i] . "</p>";
                 $html .= "</div>
                     </div>
-                    <div class='tab-cont";
-                if ($i == 0) $html .= ' show';
-                $html .= "'><div class='tab-cont-inner'>" . $fields['ttc'][$i] . "</div></div></div>";
+                    <div class='MGTZ_tab-cont";
+                if ($i == 0) $html .= ' MGTZ_show';
+                $html .= "'><div class='MGTZ_tab-cont-inner'>" . $fields['MGTZ_ttc'][$i] . "</div></div></div>";
             }
             $html .= "</div></div>";
             return $html;
@@ -209,3 +204,4 @@ function add_sc()
         }
     }
 }
+);
